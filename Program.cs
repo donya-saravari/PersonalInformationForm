d13b33e9e19294b400208cc07c28b12d66494b7d
donya <donya.saravari@gmail.com>
2025-08-16 19:38:07 +0330

    added conditions for phone number, gender and generate uniqe code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿namespace PersonalInformationForm
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace PersonalInformationForm
 {
+    enum Gender
+    {
+        Male,
+        Female
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -8,24 +15,58 @@
             string Firstname = Console.ReadLine();
             Console.WriteLine("Please enter your last name:");
             string Lastname = Console.ReadLine();
-            Console.WriteLine("Please enter your phone number:");
-            string Phonenum = Console.ReadLine();
-            int phonenumber = Int32.Parse(Phonenum);
+            string Phonenum = phoneNumber();
+            static string phoneNumber() {
+                Console.WriteLine("Please enter your phone number:");
+                string Phonenum = Console.ReadLine();
+                if (Phonenum.Length == 11 && Phonenum.StartsWith("09")) ;
+                else
+                {
+                    Console.WriteLine("Please enter valid phone number!");
+                    phoneNumber();
+                }
+                return Phonenum;
+            }
             Console.WriteLine("Please enter your national code:");
-            string Ncode = Console.ReadLine();
-            int ncode = Int32.Parse(Ncode);
-            Console.WriteLine("Please enter your gender:");
-            string Gender = Console.ReadLine();
-            Console.WriteLine("Please enter your age:");
-            string Age = Console.ReadLine();
-            int age = Int32.Parse(Age);
+            int Ncode = int.Parse(Console.ReadLine());
+            Gender gender = GetGenderFromUser();
+            static Gender GetGenderFromUser()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Please enter your gender: M->male , F->female");
+                    string GenderIn = Console.ReadLine().ToLower();
+                    if (GenderIn == "m")
+                        return Gender.Male;
+                    else if (GenderIn == "f")
+                        return Gender.Female;
+                    else
+                        Console.WriteLine("Please enter valid Gender!");
+                }
+            }
+        Console.WriteLine("Please enter your year of birth:");
+            string birth = (Console.ReadLine());
+            DateTime birthDate = DateTime.Parse(birth);
+            int age = CalculateAge(birthDate);
+           static int CalculateAge(DateTime birthDate)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - birthDate.Year;
+                return age;
+            }
+            Console.WriteLine("Please enter your city:");
+            string city = Console.ReadLine();
+
             Console.WriteLine("Thanks, Here is your information:");
             Console.WriteLine("First name: " + Firstname);
             Console.WriteLine("Last name: " + Lastname);
-            Console.WriteLine("Phone nember: " + phonenumber);
-            Console.WriteLine("National code: " + ncode);
-            Console.WriteLine("Gender: " + Gender);
+            Console.WriteLine("Phone nember: " + Phonenum);
+            Console.WriteLine("National code: " + Ncode);
+            Console.WriteLine("Gender: " + gender);
             Console.WriteLine("Age: " + age);
+            Console.WriteLine("your uniqe code: " + Guid.NewGuid().ToString());
         }
+
+
     }
 }
