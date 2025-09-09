26c2dd33d929f0fae375f5e7d97ed61bd6512ee9
donya <donya.saravari@gmail.com>
2025-09-09 13:09:44 +0330

    check gender

--- Program.cs
+++ Program.cs
@@ -4,60 +4,111 @@ namespace PersonalInformationForm
 {
     enum Gender
     {
-        Male,
-        Female
+        Male = 0,
+        Female = 1
     }
     internal class Program
     {
+        public static bool IsValidPhoneNumber(string Phonenum)
+        {
+
+            while (Phonenum.All(char.IsDigit))
+            {
+                if (Phonenum.Length == 11 && Phonenum.StartsWith("09"))
+                {
+                    return true;
+                }
+                else if (Phonenum.Length == 10 && Phonenum.StartsWith("9"))
+                {
+                    Phonenum = "0" + Phonenum;
+                    return true;
+                }
+                else if (Phonenum.Length == 13 && Phonenum.StartsWith("+98"))
+                {
+                    Phonenum = Phonenum.Replace("+98", "0");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter valid phone number!");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+       public static Gender GetGenderFromUser(string GenderIn)
+        {
+            while (true)
+            { 
+
+                if (GenderIn == "m")
+                    return Gender.Male;
+                else if (GenderIn == "f")
+                    return Gender.Female;
+                else
+                    Console.WriteLine("Please enter valid Gender!");
+            }
+        }
+        public static bool CheckGender(string genderstr)
+        {
+            if (Enum.TryParse(typeof(Gender), genderstr, true, out var gender))
+            {
+                return (Gender)gender == Gender.Male || (Gender)gender == Gender.Female;
+            }
+            else
+                return false;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter your first name:");
             string Firstname = Console.ReadLine();
             Console.WriteLine("Please enter your last name:");
             string Lastname = Console.ReadLine();
-            string Phonenum = phoneNumber();
-            static string phoneNumber()
+            Console.WriteLine("enter phone number");
+            string phonenumber;
+            for (; ; )
             {
-                    Console.WriteLine("Please enter your phone number:");
-                    string Phonenum = Console.ReadLine();
-                while (Phonenum.All(char.IsDigit))
+                try
                 {
-                    if (Phonenum.Length == 11 && Phonenum.StartsWith("09")) ;
-                    else if (Phonenum.Length == 10 && Phonenum.StartsWith("9"))
+                    phonenumber = Console.ReadLine() ?? throw new Exception("invalid phonenumber!");
+
+                    if (IsValidPhoneNumber(phonenumber))
                     {
-                        Phonenum = "0" + Phonenum;
-                    }
-                    else if (Phonenum.Length == 13 && Phonenum.StartsWith("+98"))
-                    {
-                        Phonenum = Phonenum.Replace("+98","0");
+                        break;
                     }
                     else
                     {
-                        Console.WriteLine("Please enter valid phone number!");
-                        phoneNumber();
+                        Console.WriteLine("you entered invalid phone number");
                     }
-
-                    return Phonenum;
                 }
-                return null;
+                catch(Exception ex)
+                {
+                    Console.WriteLine("you entered invalid phone number");
+                    continue;
+                }
+
             }
             Console.WriteLine("Please enter your national code:");
             int Ncode = int.Parse(Console.ReadLine());
-            Gender gender = GetGenderFromUser();
-            static Gender GetGenderFromUser()
+            string GenderIn;
+            Console.WriteLine("Please enter your gender: M->male , F->female");
+            GenderIn = Console.ReadLine().ToLower() ?? string.Empty;
+            Gender x = GetGenderFromUser(GenderIn);
+            for (; ; )
             {
-                while (true)
+                if (x is not (Gender)0 or not (Gender)1)
                 {
                     Console.WriteLine("Please enter your gender: M->male , F->female");
-                    string GenderIn = Console.ReadLine().ToLower();
-                    if (GenderIn == "m")
-                        return Gender.Male;
-                    else if (GenderIn == "f")
-                        return Gender.Female;
-                    else
-                        Console.WriteLine("Please enter valid Gender!");
+                    GenderIn = Console.ReadLine().ToLower() ?? string.Empty;
+                    x = GetGenderFromUser(GenderIn);
                 }
+                else
+                    break;
             }
+
             Console.WriteLine("Please enter your year of birth:");
             string birth = (Console.ReadLine());
             DateTime birthDate = DateTime.Parse(birth);
@@ -74,13 +125,13 @@ namespace PersonalInformationForm
             Console.WriteLine("Thanks, Here is your information:");
             Console.WriteLine("First name: " + Firstname);
             Console.WriteLine("Last name: " + Lastname);
-            Console.WriteLine("Phone nember: " + Phonenum);
+            Console.WriteLine("Phone nember: " + phonenumber);
             Console.WriteLine("National code: " + Ncode);
-            Console.WriteLine("Gender: " + gender);
+            Console.WriteLine("Gender: " + GenderIn);
             Console.WriteLine("Age: " + age);
             Console.WriteLine("your uniqe code: " + Guid.NewGuid().ToString());
         }
+ 
 
-
-    }
+}
 }
